ecb00e377334334df5b98d53422ab3e9ed2b3f55
shan <122284309+shanboy123@users.noreply.github.com>
2024-07-20 01:39:12 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,7 +16,7 @@ travaSend: process.env.TRAVA_SEND || '25' ,
 MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
 OWNER_NAME: process.env.OWNER_NAME || 'VAJIRA' ,
-COMMAND_TYPE: process.env.COMMAND_TYPE || 'button' ,
+COMMAND_TYPE: process.env.COMMAND_TYPE || 'nonbutton' ,
 LOGO: process.env.LOGO || `https://telegra.ph/file/ab0ea6ad90e2eb98090d9.jpg` ,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgres://vajiratech_user:oSIFl2xmSojMZ0rkzdd0g0W6msuVTpNN@dpg-cpd7fjv109ks73e5gtig-a.frankfurt-postgres.render.com/vajiratech' : process.env.POSTGRESQL_URL,
 FOOTER: process.env.FOOTER || '‌👨‍💻 ＶＡＪＩＲＡ -  ＭＤ - Ｖ5 👨‍💻',    
